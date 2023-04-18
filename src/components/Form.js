3d74749fc8d0701c4a85e820dcd35130f7c5fb7d
Renata Reinartz <rcreinartz@gmail.com>
2023-04-18 09:45:43 -0600

    pseudo code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,36 @@
+// Define priceOption and productType constant variables.
 export const priceOption = {
-    oneToTen: '1-10',
-    tenToFifteen: '10-15',
-    fifteenToThirty: '15-30'
-}
+  oneToTen: '1-10',
+  tenToFifteen: '10-15',
+  fifteenToThirty: '15-30',
+};
 
 export const productType = {
-	eyeshadow: 'eyeshadow',
-	eyeliner: 'eyeliner',
-	powder: 'powder',
-	foundation: 'foundation',
-	lipstick: 'lipstick'
-}
-
+  eyeshadow: 'eyeshadow',
+  eyeliner: 'eyeliner',
+  powder: 'powder',
+  foundation: 'foundation',
+  lipstick: 'lipstick',
+};
+// Render a form element with onSubmit event listener calling props.handleFormSubmit
 const Form = (props) => (
   <form onSubmit={props.handleFormSubmit}>
+    {/* Render a div element with class box */}
     <div className="box">
+      {/* Render two select elements inside the div element with className
+      orientationPicker */}
       <select
         value={props.userChoice}
         className="orientationPicker"
         onChange={props.handleFormChange}
       >
-				{ Object.entries(productType).map(([key, value]) => <option key={key} value={value}>{value}</option>) }
+        {Object.entries(productType).map(([key, value]) => (
+          <option key={key} value={value}>
+            {value}
+          </option>
+        ))}
       </select>
+      {/* Render two select elements with classes orientationPicker: */}
       <select
         value={props.userPrice}
         className="orientationPicker"
--- src/contexts/cart.js
+++ src/contexts/cart.js
@@ -1,47 +1,53 @@
-import { createContext, useContext, useEffect, useState } from "react";
-
+// Import required dependencies
+import { createContext, useContext, useEffect, useState } from 'react';
+// Create a cart context using createContext
 const cartContext = createContext({});
+// Export a CartProvider component that takes children as props
+export const CartProvider = ({ children }) => {
+  // Initialize cart state using useState
+  const [cart, setCart] = useState([]);
+  // Define a function to add products to the cart
+  const addToCart = (product, amount) => {
+    // Use setCart to update the cart state
+    setCart((currentCart) => {
+      // Find the index of the product in the cart
+      const index = currentCart.findIndex((x) => x.product.id === product.id);
+      const productCartInfo = index > -1 ? currentCart.splice(index, 1)[0] : {};
 
-export const CartProvider = ({children}) => {
-	const [cart, setCart] = useState([]);
+      productCartInfo.product = product;
+      productCartInfo.amount = amount;
 
-	const addToCart = (product, amount) => {
-		setCart(currentCart => {
-			const index = currentCart.findIndex(x => x.product.id === product.id);
-			const productCartInfo = index > -1 ? currentCart.splice(index, 1)[0] : {};
-				
-			productCartInfo.product = product;
-			productCartInfo.amount = amount;
+      return [...currentCart, productCartInfo];
+    });
+  };
+  // Define a function to remove products from the cart
+  const removeFromCart = (product) => {
+    // Use setCart to update the cart state
+    setCart((currentCart) => {
+      // Use filter to remove the product from the cart
+      currentCart.filter((x) => x.product.id === product.id);
+      // Return a new array with the updated cart
+      return [...currentCart];
+    });
+  };
+  // Use useEffect to save the cart to local storage whenever it changes
+  useEffect(() => {
+    const startingCart = localStorage.getItem('cart');
 
-			return [...currentCart, productCartInfo];
-		});
-	}
+    if (!!startingCart) {
+      setCart(JSON.parse(startingCart));
+    }
+  }, []);
 
-	const removeFromCart = (product) => {
-		setCart(currentCart => {
-			currentCart.pop(x => x.product.id === product.id);
-
-			return [ ...currentCart ];
-		});
-	}
-
-	useEffect(() => {
-		const startingCart = localStorage.getItem('cart');
-		
-		if (!!startingCart) {
-			setCart(JSON.parse(startingCart));
-		}
-	}, []);
-
-	useEffect(() => {
-		localStorage.setItem('cart', JSON.stringify(cart))
-	}, [cart]);
-
-	return (
-		<cartContext.Provider value={{cart, addToCart, removeFromCart}}>
-			{children}
-		</cartContext.Provider>
-	)
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+  // Render the cart context provider with the cart, addToCart, and removeFromCart values
+  return (
+    <cartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </cartContext.Provider>
+  );
 };
 
 export const useCart = () => useContext(cartContext);
